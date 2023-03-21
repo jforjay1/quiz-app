9a9615cdc873530930b50b1a797ec414e734ca24
Jay Patel <46365760+jforjay1@users.noreply.github.com>
2023-03-21 10:24:31 -0300

    -- 0 Bugs -- 3 security hotspots -- 5 code smells -- 59min in debt

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Play from './components/quiz/Play';
 import QuizSummary from './components/quiz/QuizSummary';
 
 function App() {
-  return (
-    <Router>
-      <Route path="/" exact component={Home} />
-      <Route path="/play/quiz" exact component={Play} />
-      <Route path="/play/quizSummary" exact component={QuizSummary} />
-    </Router>
+	return (
+		<Router>
+			<Route path="/" exact component={Home} />
+			<Route path="/play/quiz" exact component={Play} />
+			<Route path="/play/quizSummary" exact component={QuizSummary} />
+		</Router>
 
-  );
+	);
 }
 
 export default App;
--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,21 +1,21 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { mount } from 'enzyme';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {mount} from 'enzyme';
 import App from './App';
 import Home from '../components/Home';
 import Play from '../components/quiz/Play';
 import QuizSummary from '../components/quiz/QuizSummary';
 
 describe('<App />', () => {
-    it('renders the correct routes', () => {
-        const wrapper = mount(
-            <Router>
-                <App />
-            </Router>
-        );
+	it('renders the correct routes', () => {
+		const wrapper = mount(
+			<Router>
+				<App />
+			</Router>
+		);
 
-        expect(wrapper.find(Route)).toHaveLength(3);
-        expect(wrapper.find({ path: '/' }).props().component).toEqual(Home);
-        expect(wrapper.find({ path: '/play/quiz' }).props().component).toEqual(Play);
-        expect(wrapper.find({ path: '/play/quizSummary' }).props().component).toEqual(QuizSummary);
-    });
+		expect(wrapper.find(Route)).toHaveLength(3);
+		expect(wrapper.find({ path: '/' }).props().component).toEqual(Home);
+		expect(wrapper.find({ path: '/play/quiz' }).props().component).toEqual(Play);
+		expect(wrapper.find({ path: '/play/quizSummary' }).props().component).toEqual(QuizSummary);
+	});
 });
