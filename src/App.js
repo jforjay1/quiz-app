d4e785215790f445ff9518cdca9fb26bd80ada37
Jay Patel <46365760+jforjay1@users.noreply.github.com>
2024-01-10 17:40:55 -0400

    Added json server file
    Updated question and added json server file

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './components/Home';
 import Play from './components/quiz/Play';
 import QuizSummary from './components/quiz/QuizSummary';
-import Summary from './components/quiz/Summary';
-import Instructions from './components/quiz/Instructions';
 import LoginPage from './components/quiz/Login';
 
 function App() {
@@ -13,8 +11,6 @@ function App() {
 			<Route path="/" exact component={Home} />
 			<Route path="/play/quiz" exact component={Play} />
 			<Route path="/play/quizSummary" exact component={QuizSummary} />
-			<Route path="/play/summary" exact component={Summary} />
-			<Route path="/instructions" exact component={Instructions} />
 			<Route path="/login" exact component={LoginPage} />
 		</Router>
 
